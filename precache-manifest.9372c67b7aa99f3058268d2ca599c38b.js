db3f0f34fdc81dce318764929a53a09049b0f726
Johnny Reyes <jcreyes.student@asiancollege.edu.ph>
2022-01-09 07:59:33 +0800

    Updates

---
+++ precache-manifest.9372c67b7aa99f3058268d2ca599c38b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc39d89529030e994a9b5320d5dfa3c7",
+    "url": "/gamezone/index.html"
+  },
+  {
+    "revision": "294e0057f43427217bb0",
+    "url": "/gamezone/static/css/main.bef9be39.chunk.css"
+  },
+  {
+    "revision": "76320276ca375ea8eab1",
+    "url": "/gamezone/static/js/2.61c16d35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gamezone/static/js/2.61c16d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294e0057f43427217bb0",
+    "url": "/gamezone/static/js/main.79ef61cc.chunk.js"
+  },
+  {
+    "revision": "dcaca819b6c6e3dff041",
+    "url": "/gamezone/static/js/runtime-main.a5ec7f9c.js"
+  },
+  {
+    "revision": "2832e3034d54fc28b4a23f59c34d197e",
+    "url": "/gamezone/static/media/ACcampus.2832e303.jpg"
+  },
+  {
+    "revision": "f14a7a505b0018800f438aefb28629b4",
+    "url": "/gamezone/static/media/acsat.f14a7a50.png"
+  }
+]);
